74be170058438948993062776f179bb73c8490f8
Barath Vedantham <vedant.barath@gmail.com>
2025-03-05 10:19:24 -0800

    Add HTML response handling and static files for web server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::fs;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\
-                   Content-Type: text/html\r\n\
-                   \r\n\
-                   <html><body><h1>Hello from Rust!</h1></body></html>";
+    let contents = fs::read_to_string("static/index.html").unwrap_or_else(|_| {
+        fs::read_to_string("static/error.html")
+            .expect("Error page not found!")
+    });
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{}",
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
